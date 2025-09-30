fac568c4850b3271f3fcedf188b3c5b62df70b74
Janssen Gallenero <jansseng3@nycstudents.net>
2025-09-30 12:31:04 -0400

    page 6; review pages 4/5

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -17,17 +17,55 @@ public class AppointmentBook {
                 if(block == duration) {
                     return i - duration + 1;
                 }
+                else {
+                    block = 0;
+                }
             }
-        return 0;
+        return -1;
     }
 
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        for(int i = 25; i < 30; i++) schedule[1][i] = true;
+        for(int i = 0; i < 15; i++) schedule[2][i] = true;
+        for(int i = 41; i < 60; i++) schedule[2][i] = true;
+        for(int i = 5; i < 30; i++) schedule[1][i] = true;
+        for(int i = 44; i < 60; i++) schedule[3][i] = true;
+
+        AppointmentBook b = new AppointmentBook(schedule);
+
+        int period = 2;
+        while (period < 5) {
+            System.out.println("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
+
+        for(int i = startMinute; i < startMinute + duration; i++) {
+            schedule[period - 1][i] = false;
+        }
+
+        for(int i = startPeriod; i<= endPeriod; i++) {
+            int freeBlock =  findFreeBlock(i, duration);
+        }
+
+        if (freeBlock > -1) {
+            reserveBlock(i, freeBlock, duration);
+            return true;
+
+        }
+
+
+
         return false;
     }
+
 
     public void printPeriod(int period) {
         for(int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
     }
+
+
+
 
 }
